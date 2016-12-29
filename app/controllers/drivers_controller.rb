c4be7815b04f2cce83440c40c878e7553b413454
Prelang Builder <builder@prelang.com>
2016-12-29 20:30:49 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,4 +1,8 @@
 class DriversController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_driver, only: [:show, :edit, :update, :destroy]
 
   # GET /drivers
