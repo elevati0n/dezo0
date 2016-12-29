763dbee325f3ffdc4e57f896feee2759efdb9c89
Prelang Builder <builder@prelang.com>
2016-12-29 20:30:55 +0000

    Generated scaffolding for model 'Ride' via Rails

---
+++ app/views/rides/_ride.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! ride, :id, :job_id, :passenger_id, :created_at, :updated_at
+json.url ride_url(ride, format: :json)
---
+++ test/controllers/rides_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class RidesControllerTest < ActionController::TestCase
+  setup do
+    @ride = rides(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:rides)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create ride" do
+    assert_difference('Ride.count') do
+      post :create, ride: { job_id: @ride.job_id, passenger_id: @ride.passenger_id }
+    end
+
+    assert_redirected_to ride_path(assigns(:ride))
+  end
+
+  test "should show ride" do
+    get :show, id: @ride
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @ride
+    assert_response :success
+  end
+
+  test "should update ride" do
+    patch :update, id: @ride, ride: { job_id: @ride.job_id, passenger_id: @ride.passenger_id }
+    assert_redirected_to ride_path(assigns(:ride))
+  end
+
+  test "should destroy ride" do
+    assert_difference('Ride.count', -1) do
+      delete :destroy, id: @ride
+    end
+
+    assert_redirected_to rides_path
+  end
+end
