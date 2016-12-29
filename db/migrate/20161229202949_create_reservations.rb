21c86422ed44eacb62d4841ca5c49eb616763e6a
Prelang Builder <builder@prelang.com>
2016-12-29 20:29:49 +0000

    Generated model Reservation with migration

---
+++ db/migrate/20161229202949_create_reservations.rb
@@ -0,0 +1,9 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+      t.references :user, index: true
+
+      t.timestamps
+    end
+  end
+end
